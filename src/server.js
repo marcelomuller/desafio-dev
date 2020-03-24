9b86d977adba3a8651bac3f437012ee6d38545da
Marcelo Muller <marcelo_mvf@hotmail.com>
2020-03-23 22:36:27 -0300

    Changing server stuff

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.urlencoded());
 
 app.set('view engine', 'ejs');
@@ -19,7 +21,9 @@ app.post('/resultado', (req, res) => {
     res.render("resultado", { divisors: calcResult[0], prime: calcResult[1] });
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+  });
 
 function calcDivisors(num) {
     let res = [];
